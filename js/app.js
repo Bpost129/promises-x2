d6c22c670f81842f789a04752d7523f36e14730a
Bpost129 <Bpost129@yahoo.com>
2025-12-16 14:06:20 -0500

    create an async function that returns a promise to resolve the sum of 2 numbers after a delay, chain promises

--- js/app.js
+++ js/app.js
@@ -1,27 +1,46 @@
-const p = new Promise(function(resolve, reject) {
+// const p = new Promise(function(resolve, reject) {
 // synchronous promise  
   // console.log(resolve, reject)
-  let value = 41
-  resolve(value)
+  // let value = 41
+  // resolve(value)
 
 // asynchronous promise
   // setTimeout(function() {
   //   reject('Time is up!')
   // }, 2000)
-})
+// })
 
-p.then(function(result) {
-  console.log(result, '<-- first .then')
-  return 42
-})
-.then(function(result) {
-  console.log(result, '<-- second .then')
-  return 'Done!'
-})
-.then(function(result) {
-  console.log(result, '<-- third .then')
-})
+// p.then(function(result) {
+//   console.log(result, '<-- first .then')
+//   return 42
+// })
+// .then(function(result) {
+//   console.log(result, '<-- second .then')
+//   return 'Done!'
+// })
+// .then(function(result) {
+//   console.log(result, '<-- third .then')
+// })
 
 // .catch(function(err) {
 //   console.log(err)
 // })
+
+
+function asyncAdd(a, b, delay) {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve(a + b)
+    }, delay)
+  })
+}
+
+
+asyncAdd(2, 3, 2000)
+.then(function(sum) {
+  console.log(sum)
+  return asyncAdd(sum, 100, 1000)
+})
+.then(function(sum) {
+  console.log(sum)
+})
